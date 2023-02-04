351cf528583ffd1c6decf1382e335df56944a89e
login4 <birukovartemij@gmail.com>
2023-02-04 14:41:48 +0300

    task2 completed

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число m: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int FindSumOfNatural(int m, int n)
+{
+    int sum = 0;
+    if (m<n && m > 0 && n > 0)
+    {
+        sum = m + FindSumOfNatural(m+1, n);
+        return sum;
+    }
+    else 
+    {
+        sum = sum + n;
+        return sum;
+    }
+}
+
+Console.WriteLine($"Сумма: {FindSumOfNatural(m, n)}");
